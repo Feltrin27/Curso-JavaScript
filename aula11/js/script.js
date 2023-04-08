332805d8889d5478cf0b23dfda4de7685bcfb601
Feltrin27 <117307790+Feltrin27@users.noreply.github.com>
2023-04-08 17:09:43 -0300

    Exercicios com String e number

---
+++ aula11/js/script.js
@@ -0,0 +1,9 @@
+//alert('Com a nossa mensagem.')
+//confirm('Deseja realmente apagar?')
+//prompt('Digite o seu nome!')
+
+let num1 = prompt('Digite um número');
+let num2 = prompt('Digite outro número');
+num1 = Number(num1);
+num2 = Number(num2);
+alert(`O resultado foi ${num1 + num2}`);
