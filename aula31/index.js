081f5f9cfc7c347fc6fba3e10d434fb2a245b18b
Feltrin27 <117307790+Feltrin27@users.noreply.github.com>
2023-04-25 11:31:09 -0300

    Estrutura de repetição, for clássico

---
+++ aula31/index.js
@@ -0,0 +1,9 @@
+for (let i = 0; i <= 5; i++) {
+    const par = i % 2 === 0 ? 'par' : 'impar';
+    console.log(i , par );
+}
+
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Banana', 'Melancia', 'Caju', 'Morango'];
+for (let i = 0; i < frutas.length; i++) {
+ console.log(`Índice ${i}`, frutas[i]);    
+}
