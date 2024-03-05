3d83b0a0cefd254d08a997e8e1643cdd670b0107
Feltrin27 <117307790+Feltrin27@users.noreply.github.com>
2024-03-04 21:13:22 -0300

    filter

---
+++ aula57 filter/aula57.js
@@ -0,0 +1,28 @@
+//FILTER VAI SEMPRE RETORNAR UM ARRAY, COM A MESMA QUANTIDADE DE ELEMENTOS OU MENOS.
+
+//Retorne os números maiores que 10+
+//               0   1   2  3  4  5  6  7  8   9  10  11  12
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const numerosFiltrados = numeros.filter(valor => valor > 10); // função de callback filter com arrowfunction busca só o valor fazendo um for no array
+console.log(numerosFiltrados);
+
+//Retorne as pessoas que tem o nome com 5 letras ou mais
+//Retorne as pessoas com mais de 50 anos
+//Retorne as pessoas cujo nome termina com a
+const pessoas = [
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Maria', idade: 23},
+    {nome: 'Eduardo', idade: 55},
+    {nome: 'Letícia', idade: 19},
+    {nome: 'Rosana', idade: 32},
+    {nome: 'Wallace', idade: 47},
+];
+
+const pessoasComNomeGrande = pessoas.filter(obj => obj.nome.length >= 5);
+console.log(pessoasComNomeGrande);
+const mais50 = pessoas.filter(objeto => objeto.idade > 50);
+console.log(mais50);
+const nomeTerminaA = pessoas.filter(obj => {
+    return obj.nome.toLowerCase().endsWith('a');
+});
+console.log(nomeTerminaA);
