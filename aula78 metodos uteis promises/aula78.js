59b6304ec758e86a676355a63561f8675a3284f4
Feltrin27 <117307790+Feltrin27@users.noreply.github.com>
2024-03-27 11:22:06 -0300

    Promise.all Promise.race

--- aula78 metodos uteis promises/aula78.js
+++ aula78 metodos uteis promises/aula78.js
@@ -6,9 +6,53 @@ function rand(min, max) {
 
 function esperaAi(msg, tempo) {
     return new Promise((resolve, reject) => {
-        if(typeof msg !== 'string') reject('BAD VALUE');
+        if (typeof msg !== 'string') {
+            reject('Cai no erro!');
+            return;
+        }
         setTimeout(() => {
-            resolve(msg);
+            resolve(msg.toUpperCase() + ' - Passei na promise');
         }, tempo);
     });
 }
+
+// Promise.all
+const promises = [
+    //'Primeiro valor',
+    esperaAi('Promise 1', rand(1, 5)),
+    esperaAi('Promise 2', rand(1, 5)),
+    esperaAi('Promise 3', rand(1, 5)),
+    //esperaAi(1000, 1000),
+    //'Outro valor'
+];
+// se tiver erro em uma dessas promessas ele para e não entrega nenhuma
+Promise.all(promises)
+    .then(function (valor) {
+        console.log(valor);
+    }).catch(function (erro) {
+        console.log(erro);
+    });
+
+//Entrega o primeiro a ser executado!
+Promise.race(promises)
+    .then(function (valor) {
+        console.log(valor);
+    }).catch(function (erro) {
+        console.log(erro);
+    });
+
+function baixaPagina() {
+    const emCache = false;
+
+    if (emCache) {
+        return Promise.resolve('Página em cache');
+    } else {
+        return esperaAi('Baixei a página', 3000);
+    }
+}
+
+baixaPagina()
+.then(dadosPagina => {
+    console.log(dadosPagina);
+})
+.catch(e => console.log(e));
