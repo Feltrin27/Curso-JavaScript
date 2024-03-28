5efc4748918e25b0edc02de5137a72cd2460c87c
Feltrin27 <117307790+Feltrin27@users.noreply.github.com>
2024-03-28 12:12:45 -0300

    XML REQUEST

---
+++ aula81 FETCH API/assets/js/main.js
@@ -0,0 +1,49 @@
+const request = obj => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest(); //cria requisição com o construtor 
+        xhr.open(obj.method, obj.url, true);
+        xhr.send();
+
+        xhr.addEventListener('load', () => {  // verifica o estado da página para ver 
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.responseText);
+            } else {
+                reject(xhr.statusText);
+            }
+        });
+    });
+
+};
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault(); //previne que vá para á página do link
+        carregaPagina(el);
+    }
+});
+
+async function carregaPagina(el) {
+    const href = el.getAttribute('href');
+
+    const objConfig = {
+        method: 'GET',
+        url: href
+    };
+
+    try {
+        const response = await request(objConfig);
+        carregaResultado(response);
+        window.open(href, '_blank');
+    }catch(e){
+        console.log(e);
+    }
+   
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+}
