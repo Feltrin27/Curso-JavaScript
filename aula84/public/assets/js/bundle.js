cc2d3a861d9b9fe784ea2292775ac5019139d57f
Feltrin27 <117307790+Feltrin27@users.noreply.github.com>
2024-04-03 11:45:01 -0300

    ES6 MODULES - IMPORT/EXPORT

---
+++ aula84/public/assets/js/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log(100),console.log("Luiz"),console.log("Miranda"),console.log(30),console.log(20)})();
+//# sourceMappingURL=bundle.js.map
---
+++ aula84/src/index.js
@@ -0,0 +1,7 @@
+import multip, {nome, sobrenome, idade, soma} from './modulo1';
+
+console.log(multip(10, 10));
+console.log(nome);
+console.log(sobrenome);
+console.log(idade);
+console.log(soma(10, 10));
