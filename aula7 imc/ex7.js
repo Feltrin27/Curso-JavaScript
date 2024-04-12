9cac05388e859c1dd10ffda09cf709b4b992d974
Feltrin27 <117307790+Feltrin27@users.noreply.github.com>
2024-04-12 12:47:50 -0300

    Revisão

--- aula7 imc/ex7.js
+++ aula7 imc/ex7.js
@@ -1,6 +1,6 @@
 const nome = 'Erivelton';
 const sobrenome = 'Feltrin';
-const idade = 32;
+const idade = 33;
 const peso = 80;
 const altura = 1.80;
 let imc; // peso / (altura * altura)
@@ -11,5 +11,5 @@ imc = peso / (altura * altura);
 anoNascimento = anoAtual - idade;
 
 console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg, 
-tem ${altura} metros de altura e seu IMC é de ${imc} e ${nome} ${sobrenome} nasceu em ${anoNascimento} `)
+tem ${altura} metros de altura e seu IMC é de ${imc.toFixed(2)} e ${nome} ${sobrenome} nasceu em ${anoNascimento} `)
 
