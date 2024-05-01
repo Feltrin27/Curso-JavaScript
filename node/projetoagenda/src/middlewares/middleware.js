03c1bac32ef1ed7e676f84679a7c177a0ff85a38
Feltrin27 <117307790+Feltrin27@users.noreply.github.com>
2024-05-01 17:14:11 -0300

    Criando mensagens de sucesso e criado primeiros usuários

--- node/projetoagenda/src/middlewares/middleware.js
+++ node/projetoagenda/src/middlewares/middleware.js
@@ -1,5 +1,6 @@
 exports.middlewareGlobal = (req, res, next) => {
-    res.locals.umaVariavelLocal = 'Este é o valor da variável local.';
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
     next();
 };
 
@@ -11,6 +12,7 @@ exports.checkCsrfError = (err, req, res, next) => {
     if(err){
         return res.render('404');
     }
+
     next();
 };
 
