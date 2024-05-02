51c9d29533ba1c84c81ce736f141431b40bf6d70
Feltrin27 <117307790+Feltrin27@users.noreply.github.com>
2024-05-02 14:55:27 -0300

    Hash de Senha
    Hash de senha + não permitir redundância de usuário.

--- node/projetoagenda/src/models/LoginModel.js
+++ node/projetoagenda/src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 //Classe home exemplo
 const LoginSchema = new mongoose.Schema({
@@ -17,26 +18,41 @@ class Login {
     }
 
     async register() {
+        //Método de ver se os campos estão preenchidos corretamente
         this.valida();
-        if(this.errors.length > 0) return;
-        try{
+        //Aqui se tiver erros dentro do array errors return o erro
+        if (this.errors.length > 0) return;
+        //Verificação se usuário já existe na base de dados para evitar redundância
+        await this.userExists();
+        //Verifica novamente se array de errors está vazio
+        if (this.errors.length > 0) return;
+        //Cria um salt
+        const salt = bcryptjs.genSaltSync();
+        //Faz um Hash da senha preenchida
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+        try {
             this.user = await LoginModel.create(this.body);
-        }catch(e){
+        } catch (e) {
             console.log(e);
         };
     }
-
+    //Método para verificar se usuário já está cadastrado
+    async userExists(){
+      const user = await LoginModel.findOne({email: this.body.email});
+      if(user) this.errors.push('Usuário já existe.');
+    }
+    //Método para verificar se campos atendem aos requisitos
     valida() {
         this.cleanUp();
         //validação
         //o email precisa ser válido
-        if(!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
+        if (!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
         // a senha precisa ter entre 3 e 50
-        if(this.body.password.length < 3 || this.body.password.length > 50){
+        if (this.body.password.length < 3 || this.body.password.length > 50) {
             this.errors.push('A senha precisa ter entre 3 e 50 caracteres.');
         }
     }
-
+    //Método para verificar se é uma string
     cleanUp() {
         for (const key in this.body) {
             if (typeof this.body[key] !== 'string') {
