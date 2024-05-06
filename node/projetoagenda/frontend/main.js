3470b61251cbe16977535cfc307772762ee9b95d
Feltrin27 <117307790+Feltrin27@users.noreply.github.com>
2024-05-06 13:04:57 -0300

    tratando erros no front-end

--- node/projetoagenda/frontend/main.js
+++ node/projetoagenda/frontend/main.js
@@ -1,4 +1,10 @@
 import 'core-js/stable'; //Biblioteca para compatibilidade de navegadores mais antigos
 import 'regenerator-runtime/runtime'; // Dependência do babel para suportar funcionalidades es6+
-import './assets/css/style.css'; //Import css forma moderna
+import Login from './modules/Login';
+
+const login = new Login('.form-login');
+const cadastro = new Login('.form-cadastro');
+login.init();
+cadastro.init();
+//import './assets/css/style.css'; //Import css forma moderna
 
