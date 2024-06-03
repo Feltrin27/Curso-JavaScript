8fcb10baefff3fd04a6cc34958d1920f26195c5a
Erivelton Feltrin <erivelton.feltrin@gmail.com>
2024-06-03 18:12:07 -0300

    atualizações para delete put e index

--- API_REST/src/controllers/HomeController.js
+++ API_REST/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Rosana',
-      sobrenome: 'Feltrin',
-      email: 'teste2@gmail.com',
-      idade: 33,
-      peso: 56,
-      altura: 1.7,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
