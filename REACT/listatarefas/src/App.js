3bb15ca418025f08b1b194d5070df5c81dbccf09
Erivelton Feltrin <erivelton.Feltrin@gmail.com>
2024-07-03 11:41:42 -0300

    lista de tarefas react

--- REACT/listatarefas/src/App.js
+++ REACT/listatarefas/src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
+import Main from './components/Main';
 import './App.css';
 
 export default function App() {
-  return (
-    <>
-      <h1>Olá Mundo!</h1>
-      <p>Lorem ipsum dolor sit amet.</p>
-    </>
-  );
+  return <Main />;
 }
