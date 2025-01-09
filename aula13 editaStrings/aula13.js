daee3fdbf4b82395d61a4ecc10fe1fa1a2009aec
Erivelton Feltrin <erivelton.Feltrin@gmail.com>
2025-01-09 11:16:17 -0300

    Update aula13.js

--- aula13 editaStrings/aula13.js
+++ aula13 editaStrings/aula13.js
@@ -1,4 +1,27 @@
 //               01234567
-let umaString = "Um texto";
+let umaString = "O rato roeu a roupa do rei de roma.";
 
+console.log(umaString.concat(' ', 'em', ' ', 'um', ' ', 'lindo', ' ', 'dia'));
+console.log(umaString + ' em um lindo dia.');
+console.log(`${umaString} em um lindo dia.`)
+
+
+console.log(umaString.charAt(6));
+console.log(umaString.indexOf('o', 3))
 console.log(umaString.indexOf('texto'));
+console.log(umaString.lastIndexOf('m', 3));
+console.log(umaString[4]);
+
+console.log(umaString.match(/[a-z]/));
+console.log(umaString.search(/[a-z]/));
+console.log(umaString.replace(/r/g, 'l'));
+
+console.log(umaString.length);
+console.log(umaString.length - 3);
+console.log(umaString.slice(-5, -1));
+console.log(umaString.slice(2, 6));
+
+console.log(umaString.substring(umaString.length -5));
+console.log(umaString.split(' ', 2));
+console.log(umaString.toLocaleUpperCase());
+console.log(umaString.toLocaleLowerCase());
